cf68d0c36e6397283692d84671d036b67b779da7
Latricha Seym <latricha.seym@hva.nl>
2026-03-17 11:56:52 +0100

    add basic components

--- client/src/components/ui/Header.tsx
+++ client/src/components/ui/Header.tsx
@@ -1,8 +1,32 @@
+import { Link } from "react-router";
+
 export default function Header() {
   return (
     <header>
-      <div></div>
-      <nav></nav>
+      <div>
+        Volg ons op:
+        <ul>
+          <li>I</li>
+          <li>F</li>
+          <li>S</li>
+        </ul>
+      </div>
+      <div className="flex">
+        <p>Logo</p>
+        <div>
+          <p>NL</p>
+        </div>
+      </div>
+      <nav>
+        <div>
+          <Link to="">About</Link>
+          <Link to="">Blog</Link>
+          <Link to="">Faq</Link>
+        </div>
+        <div>
+          <Link to="">Contact</Link>
+        </div>
+      </nav>
     </header>
   );
 }
