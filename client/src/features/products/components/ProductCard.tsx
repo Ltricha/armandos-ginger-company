1570e0db6fc279cb1fe8d23755847a67b6189d7a
Latricha Seym <latricha.seym@hva.nl>
2026-05-14 17:30:33 +0200

    style: add container styling to products

--- client/src/features/products/components/ProductCard.tsx
+++ client/src/features/products/components/ProductCard.tsx
@@ -1,7 +1,7 @@
 export default function ProductCard() {
   return (
-    <div>
-      <img src="" alt="" />
+    <div className="flex flex-col gap-2">
+      <img className="rounded" src="https://placehold.co/180" alt="" />
       <span>Title</span>
       <span>9,99</span>
     </div>
--- client/src/features/products/pages/Products.tsx
+++ client/src/features/products/pages/Products.tsx
@@ -3,12 +3,20 @@ import ProductCard from "../components/ProductCard";
 export default function Products() {
   return (
     <main>
-      <h1>Welcome op de homepagina.</h1>
-      <p>Lorem impsum</p>
+      <section className="py-5">
+        <div className="max-w-5xl mx-auto">
+          <h2>Products</h2>
+        </div>
+      </section>
+      <section className="py-5">
+        <div className="max-w-5xl mx-auto">
+          <div className="flex justify-between">
+            <ProductCard />
+            <ProductCard />
 
-      <section>
-        <div className="container mx-auto">
-          <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
         </div>
       </section>
     </main>
