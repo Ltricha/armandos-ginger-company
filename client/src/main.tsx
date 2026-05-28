f707416c5208b4aaea8ff4ce31cfc76f39710194
Latricha Seym <latricha.seym@hva.nl>
2026-05-28 16:21:21 +0200

    fix: homepage path

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router";
 import "./index.css";
 import Header from "./shared/components/ui/Header.tsx";
 import MobileMenu from "./shared/components/ui/MobileMenu.tsx";
-import Home from "./pages/Home.tsx";
+import Home from "./features/home/pages/Home.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
