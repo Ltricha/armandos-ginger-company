d42eb8c90a1850e6bc21d4a3fa86b4bc212ce4fb
Latricha Seym <latricha.seym@hva.nl>
2026-06-10 14:53:24 +0200

    feat: complete modal window

--- client/src/features/cart/components/CartModal.tsx
+++ client/src/features/cart/components/CartModal.tsx
@@ -1,23 +1,72 @@
 import { useModalStore } from "../store/useModalCartStore";
 import { useRef } from "react";
+import { Link } from "react-router";
+import CloseIcon from "../../../shared/components/icons/CloseIcon";
 
 export default function CartModal() {
   const isOpen = useModalStore((state) => state.isOpen);
+  const closeModal = useModalStore((state) => state.closeModal);
   const content = useModalStore((state) => state.content);
+
+  const windowContent = useRef(null);
+
+  function handleOnClick(e: React.MouseEvent<HTMLUnknownElement>) {
+    console.log(e);
+    // closeModal();
+    if (
+      windowContent.current &&
+      !windowContent.current.contains(e.target as Node)
+    ) {
+      closeModal();
+    }
+  }
 
   return (
     <div
-      className={`bg-black/50 w-full h-full fixed top-0 left-0 ${isOpen ? "block" : "hidden"}`}
+      className={`bg-black/50 w-full h-full fixed top-0 left-0 transition-opacity ${isOpen ? "block" : "hidden"}`}
+      onClick={handleOnClick}
     >
-      <div className="flex justify-between p-10 fixed w-200 min-h-120 top-1/2 left-1/2 -translate-1/2 bg-(--bg-color-primary) rounded">
-        <div className="w-1/2">
-          <h2>{content?.title} is toegevoegd aan de winkelwagen</h2>
+      <div
+        ref={windowContent}
+        className="p-10 fixed w-200 top-1/2 left-1/2 -translate-1/2 bg-(--bg-color-primary) rounded"
+      >
+        <div className="mb-5">
+          <button
+            className="absolute top-10 right-10 cursor-pointer"
+            onClick={closeModal}
+          >
+            <CloseIcon />
+          </button>
+          <h3 className="text-lg font-medium text-(--secondary-color)">
+            <span className="font-bold"> {content?.title}</span> is aan je
+            winkelwagen toegevoegd
+          </h3>
         </div>
-        <hr />
-        <div className="flex flex-col justify-center gap-10">
-          <button className="w-full p-2 bg-(--primary-color)">Afrekenen</button>
-          <hr />
-          <button className="w-full p-2">Ga verder met winkelen</button>
+        <div className="flex justify-between">
+          <div className="w-2/3">
+            <div className="flex justify-between items-center">
+              <img className="w-40" src={content?.images[0]} alt="" />
+              <p className="text">{content?.description}</p>
+            </div>
+          </div>
+          <div className="flex flex-col justify-center gap-5">
+            <Link to="/cart">
+              <button
+                className="w-full p-2 text-white font-medium bg-(--primary-color) hover:bg-(--secondary-color) rounded cursor-pointer"
+                onClick={closeModal}
+              >
+                Bekijk de winkelwagen
+              </button>
+            </Link>
+
+            <hr />
+            <button
+              className="w-full p-2 bg-white border-(--secondary-color) rounded cursor-pointer"
+              onClick={closeModal}
+            >
+              Ga verder met winkelen
+            </button>
+          </div>
         </div>
       </div>
     </div>
