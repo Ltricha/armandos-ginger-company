0eabf94cd37f095090df29350ce6a5dda433c862
Latricha Seym <latricha.seym@hva.nl>
2026-06-10 15:37:31 +0200

    feat: add delete icon

--- client/src/features/cart/components/CartItem.tsx
+++ client/src/features/cart/components/CartItem.tsx
@@ -1,6 +1,7 @@
 import type { CartItem } from "../types/CartItem";
 import QuantityStepper from "../../products/components/QuantityStepper";
 import { useCartStore } from "../store/useCartStore";
+import DeleteIcon from "../../../shared/components/icons/DeleteIcon";
 
 interface CartItemProps {
   cartItem: CartItem;
@@ -11,19 +12,28 @@ export default function CartItem({ cartItem }: CartItemProps) {
   const updateCart = useCartStore((state) => state.updateCart);
 
   return (
-    <div className="flex flex-row p-5 gap-5 shadow-md bg-(--bg-color-secondary) rounded border-b-green-700">
-      <h4>{cartItem.product.title}</h4>
-      <QuantityStepper
-        value={cartItem.quantity}
-        onChange={(e) => updateCart(cartItem.product.id, e)}
-      />
-      <button
-        onClick={() => {
-          removeFromCart(cartItem.product.id);
-        }}
-      >
-        Delete
-      </button>
+    <div className="p-5 gap-5 shadow-md bg-(--bg-color-secondary) rounded border-b-green-700">
+      <div className="flex justify-between">
+        <h3 className="mb-4 font-medium text-lg">{cartItem.product.title}</h3>
+        <button
+          onClick={() => {
+            removeFromCart(cartItem.product.id);
+          }}
+          className="text-(--primary-color)"
+        >
+          <DeleteIcon fill="currentColor" />
+        </button>
+      </div>
+      <div className="flex justify-between items-center">
+        <div>
+          <img className="max-w-40 shadow" src={cartItem.product.images[0]} />
+        </div>
+        <QuantityStepper
+          value={cartItem.quantity}
+          onChange={(e) => updateCart(cartItem.product.id, e)}
+        />
+        <p className="text-lg">{cartItem.product.price * cartItem.quantity}</p>
+      </div>
     </div>
   );
 }
